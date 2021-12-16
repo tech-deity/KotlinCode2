f5b3996aa88e9ae05d317dcdab97cb552c7ea47c
Vishal <1316510155@kit.ac.in>
2021-12-16 23:13:04 +0530

    6.1

---
+++ src/main/kotlin/KotlinEnum.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val day =Day.SUNDAY
+    println(day)
+    println(day.number)
+    for(i in Day.values())
+    {
+        println(i)
+    }
+
+    day.dayFormat()
+}
+
+enum class Day(val number:Int)
+{
+    SUNDAY(1),
+    MONDAY(2),
+    TUESDAY(3),
+    WEDNESDAY(4),
+    THURSDAY(5),
+    FRIDAY(6),
+    SATURDAY(7);
+
+    fun dayFormat(){
+        println("Day is $this")
+    }
+
+
+}
